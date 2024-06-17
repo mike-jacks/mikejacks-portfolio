112ceb1d1d5f33cffb44eb28226ca4932ea5bfa2
mjacks4816 <mjacks4816@xmail.dixietech.edu>
2024-06-17 17:51:05 +0000

    add buttons for gym-app and gym-app backend

--- src/app/Portfolio/page.tsx
+++ src/app/Portfolio/page.tsx
@@ -4,8 +4,16 @@ export default function Portfolio() {
     <>
       <div className="flex flex-col items-center gap-5 justify-center flex-grow">
         <Link href={`https://int-currency-converter.mikejacks.com/graphiql`}>
-            <button className="btn btn-primary">International Currency Converter</button>
-            </Link>
+          <button className="btn btn-primary">
+            International Currency Converter
+          </button>
+        </Link>
+        <Link href={`https://gym-app.mikejacks.com`}>
+          <button className="btn btn-primary">Gym App Front End</button>
+        </Link>
+        <Link href={`https://gym-app-backend.mikejacks.com/docs`}>
+          <button className="btn btn-primary">Gym App Back End</button>
+        </Link>
       </div>
     </>
   );
